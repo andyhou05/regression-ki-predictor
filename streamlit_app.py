58e1008cc0e130b30fa3dd583de879b35c1ad1da
Andy <andy.hou1604@gmail.com>
2024-08-15 23:12:08 -0400

    Merged conan's changes into main file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,31 @@
 import streamlit as st
-from streamlit.logger import get_logger
-
-import streamlit as st
 import pandas as pd
-import seaborn as sns
-import numpy as np
-import plotly.express as px
 import pickle
 from pandas import DataFrame
-
-import matplotlib.pyplot as plt
-
-from scipy import stats
-from sklearn import metrics
-from sklearn.cluster import KMeans
-from sklearn.linear_model import LinearRegression, LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-
+import base64
 from rdkit import Chem
+from rdkit.Chem import AllChem
 import mordred
 from mordred import Calculator, descriptors
 
-from scipy.special import inv_boxcox
-from scipy.stats import boxcox_normplot
+# Set the page configuration
+st.set_page_config(page_title="QSAR Prediction App", page_icon="📊")
+
+# Home page with explanations
+st.sidebar.title("About")
+st.sidebar.markdown("""
+### What are SMILES?
+SMILES (Simplified Molecular Input Line Entry System) is a notation that allows a user to represent a chemical structure in a way that can be used by the computer.
+
+### What is QSAR?
+Quantitative Structure-Activity Relationship (QSAR) models are used to predict the effects, properties, or activity of a compound based on its chemical structure.
+
+### Why is this important?
+Predicting the activity of compounds helps in drug discovery, reducing the cost and time of experiments.
+""")
 
 def desc_calc(smiles: str):
-    """Calculates molecular descriptors of a compound using Mordred.
-
-    Args:
-        smiles (str): SMILES notation of the compound
-
-    Returns:
-        DataFrame: A DataFrame object containing the molecular descriptors
-    """
-    
+    """Calculates molecular descriptors of a compound using Mordred."""
     mol = Chem.MolFromSmiles(smiles)
     mol = Chem.AddHs(mol)
     calc = Calculator(descriptors, ignore_3D=True)
@@ -42,58 +33,39 @@ def desc_calc(smiles: str):
     return pd.DataFrame([desc.asdict()])
 
 def model_predict(smiles: str):
-    """Predicts the pKi values against D2, D3, 5-HT1A, and 5-HT2A receptors for a given compound.
-
-    Args:
-        smiles (str): SMILES notation of the compound
-    """
-    
-    # Calculate the descriptors that will be used as features
+    """Predicts the pKi values against D2, D3, 5-HT1A, and 5-HT2A receptors for a given compound."""
     input_data = desc_calc(smiles)
-    
-    # Load the models
-    models={
-        "D2 Dopamine Receptor":pickle.load(open('./models/d2_ki.pkl', 'rb')),
-        "D3 Dopamine Receptor":pickle.load(open('./models/d3_ki.pkl', 'rb')),
-        "5-HT1A Serotonin Receptor":pickle.load(open('./models/5ht1a_ki.pkl', 'rb')),
-        "5-HT2A Serotonin Receptor":pickle.load(open('./models/5ht2a_ki.pkl', 'rb'))
-        }
-
+    models = {
+        "D2 Dopamine Receptor": pickle.load(open('./models/d2_ki.pkl', 'rb')),
+        "D3 Dopamine Receptor": pickle.load(open('./models/d3_ki.pkl', 'rb')),
+        "5-HT1A Serotonin Receptor": pickle.load(open('./models/5ht1a_ki.pkl', 'rb')),
+        "5-HT2A Serotonin Receptor": pickle.load(open('./models/5ht2a_ki.pkl', 'rb'))
+    }
     predictions = {}
     for protein, model in models.items():
-        # Select the particular descriptors that are used by the different models (they don't use the same descriptors)
         x_list = list(model.feature_names_in_)
         subset = input_data[x_list]
         predictions[protein] = model.predict(subset)[0]
-        
-    # Output the values
-    st.header('**Prediction Output (pKi Values)**')
-    st.write(pd.DataFrame(data=[predictions], index=[smiles]))
-    
-def display_dataset(dataset: DataFrame):
-    """Displays the 5 first elements and the shape of a given dataset.
+    return pd.DataFrame(data=[predictions], index=[smiles])
 
-    Args:
-        dataset (DataFrame): The dataset we want to display
-    """
-    
+def download_link(object_to_download, download_filename, download_link_text):
+    """Generates a link to download the data."""
+    b64 = base64.b64encode(object_to_download.encode()).decode()  # some strings <-> bytes conversions necessary here
+    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
+
+def display_dataset(dataset: DataFrame):
+    """Displays the 5 first elements and the shape of a given dataset."""
     st.write(dataset.head())
     st.write(dataset.shape)
-    
-    
-# UI of the Steamlit app
-st.set_page_config(
-        page_title="Medical Insurance Charges Regression",
-        page_icon="📊",
-    )
 
+# UI of the Streamlit app
 st.title('Regression')
 st.header('Raw Data')
 
 # The URL of the CSV file to be read into a DataFrame
-csv_urls = ["./data/5ht1a_mordred_ki_fingerprints.csv", 
-            "./data/5ht2a_mordred_ki_fingerprints.csv", 
-            "./data/d2_mordred_ki_fingerprints.csv", 
+csv_urls = ["./data/5ht1a_mordred_ki_fingerprints.csv",
+            "./data/5ht2a_mordred_ki_fingerprints.csv",
+            "./data/d2_mordred_ki_fingerprints.csv",
             "./data/d3_mordred_ki_fingerprints.csv"]
 
 # Reading the CSV data from the specified URL into a list of DataFrames
@@ -116,7 +88,30 @@ display_dataset(dfs[3])
 
 st.write("Enter the SMILES notation of the compound you want to predict")
 st.write("Not sure what SMILES notation is? It's a string representation of any compound made so that computers can understand them, you can find some examples of [CHEMBL](%s)" % "https://www.ebi.ac.uk/chembl/")
-smiles = st.text_input(label="SMILES", label_visibility="collapsed", placeholder="SMILES")
 
-if smiles:
-    model_predict(smiles)
+# Add drag-and-drop option for SMILES files
+uploaded_file = st.file_uploader("Upload a .txt file containing SMILES", type=["txt"])
+if uploaded_file is not None:
+    smiles_list = uploaded_file.read().decode("utf-8").splitlines()
+    st.write("SMILES loaded:")
+    st.write(smiles_list)
+    all_predictions = pd.DataFrame()
+    for smiles in smiles_list:
+        predictions = model_predict(smiles)
+        all_predictions = pd.concat([all_predictions, predictions])
+    st.header('**Prediction Output (pKi Values)**')
+    st.write(all_predictions)
+
+    # Option to download predictions
+    tmp_download_link = download_link(all_predictions.to_csv(index=False), 'predictions.csv', 'Click here to download your predictions!')
+    st.markdown(tmp_download_link, unsafe_allow_html=True)
+else:
+    # Single SMILES input option
+    smiles = st.text_input(label="SMILES", label_visibility="collapsed", placeholder="SMILES")
+    if smiles:
+        predictions = model_predict(smiles)
+        st.header('**Prediction Output (pKi Values)**')
+        st.write(predictions)
+        # Option to download the prediction
+        tmp_download_link = download_link(predictions.to_csv(index=False), 'prediction.csv', 'Click here to download your prediction!')
+        st.markdown(tmp_download_link, unsafe_allow_html=True)
